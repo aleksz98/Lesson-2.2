98d8a9cfc8f93ed73e5234f8bc9988b590442dfb
Aleksz <aleksznoob@gmail.com>
2023-03-17 21:46:42 +0100

    made traffic lights

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -14,14 +14,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenLabel: UIView!
     @IBOutlet weak var switchButton: UIButton!
     
+    var currentState: TrafficLightState = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        [redLabel, yellowLabel, greenLabel].forEach { $0.layer.cornerRadius = $0.frame.width / 2 }
+        [redLabel, yellowLabel, greenLabel].forEach { $0.layer.cornerRadius = $0.frame.size.width / 2 }
         switchButton.layer.cornerRadius = 10
     }
-
+    
+    enum TrafficLightState {
+        case red
+        case yellow
+        case green
+    }
+    
+    func changeForRed() {
+        redLabel.alpha = 1.0
+        yellowLabel.alpha = 0.3
+        greenLabel.alpha = 0.3
+        currentState = .yellow
+    }
+    
+    func changeforYellow() {
+        redLabel.alpha = 0.3
+        yellowLabel.alpha = 1.0
+        greenLabel.alpha = 0.3
+        currentState = .green
+    }
+    
+    func changeForGreen() {
+        redLabel.alpha = 0.3
+        yellowLabel.alpha = 0.3
+        greenLabel.alpha = 1.0
+        currentState = .red
+    }
+    
     @IBAction func switchButtonDidTapped(_ sender: UIButton) {
-        
+        switch currentState {
+        case .red:
+            changeForRed()
+        case .yellow:
+            changeforYellow()
+        case .green:
+            changeForGreen()
+        }
+        if currentState != .red {
+            sender.setTitle("Next", for: .normal)
+        }
     }
 }
 
