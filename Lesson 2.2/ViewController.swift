c65078299f1c3e4a753e52eeaf697680fdd1ad38
Aleksz <aleksznoob@gmail.com>
2023-03-17 13:27:16 +0100

    added outlets

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchButton(_ sender: Any) {
+        
+    }
+    
 }
 
